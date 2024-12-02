4fd8c8190bb73dac8ed45b6d62046a1e509ae0e0
HuyDNA <huydo862003@gmail.com>
2024-12-03 02:01:15 +0700

    feat: add microphone

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  boot.extraModprobeConfig = ''
+    options snd-intel-dspcfg dsp_driver=1
+  '';
+
+  environment.systemPackages = with pkgs; [
+    sof-firmware
+  ];
 }
