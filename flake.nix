e76e3d6d7b73b5db7f2bd9d5a19942f193f1b506
HuyDNA <huydo862003@gmail.com>
2024-12-28 15:24:58 +0700

    feat: add ghostty to x86_64-linux

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    ghostty.url = "github:ghostty-org/ghostty";
   };
  
-  outputs = { self, nixpkgs }: { 
+  outputs = { self, nixpkgs, ghostty }: { 
     nixosConfigurations = let
       lib = nixpkgs.lib;
       hosts = [ "x86_64-linux" ];
       hostname = "hell";
       default-username = "huydna";
+      other-pkgs = {
+        inherit ghostty;
+      };
     in lib.foldl (res: arch: lib.recursiveUpdate res {
       ${arch} = nixpkgs.lib.nixosSystem {
         system = arch;
@@ -16,6 +20,7 @@
           inherit arch;
           inherit hostname;
           inherit default-username;
+          inherit other-pkgs;
         };
         modules = [
           ./hosts
--- hosts/x86_64-linux/modules/default.nix
+++ hosts/x86_64-linux/modules/default.nix
@@ -1,5 +1,9 @@
-{ ... }: {
+{ pkgs, other-pkgs, ... }: {
   imports = [
     ./users
+  ];
+
+  environment.systemPackages = with pkgs; [
+    other-pkgs.ghostty.packages.x86_64-linux.default
   ];
 }
