f01cda40dfb68a28e936eccb0daa8dc15e9df0f0
HuyDNA <huydo862003@gmail.com>
2024-12-31 23:29:42 +0700

    feat: add sway config

--- hosts/common/modules/wayland/default.nix
+++ hosts/common/modules/wayland/default.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Window system
   programs.sway.enable = true;
   xdg.portal.wlr.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    swaylock
+    swayidle
+    swaybg
+    wofi
+    grim
+    brightnessctl
+    waybar
+  ];
 
   # Desktop environment
   services.xserver.desktopManager.gnome.enable = true;
