5c2db2c01687aacfb6abfc2af65695ae4811d45c
HuyDNA <huydo862003@gmail.com>
2024-12-31 23:32:37 +0700

    fix: remove gnome

--- hosts/common/modules/wayland/default.nix
+++ hosts/common/modules/wayland/default.nix
@@ -15,11 +15,6 @@
     waybar
   ];
 
-  # Desktop environment
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-
   # No need if enable wayland but just add it here
   services.libinput.enable = true;
 }
