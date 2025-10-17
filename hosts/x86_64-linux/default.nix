95e9d737927e30327899acb9c4fd94128fbb7a61
Huy-DNA <huydo862003@gmail.com>
2025-10-17 09:20:59 +0700

    feat: add mongodb

--- hosts/x86_64-linux/default.nix
+++ hosts/x86_64-linux/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Bootloader
@@ -15,6 +15,20 @@
   };
   networking.firewall.allowedTCPPorts = [ 2377 7946 4789 ];
   networking.firewall.allowedUDPPorts = [ 2377 7946 4789 ];
+
+  # Enable mongod
+  services.mongodb = {
+    enable = true;
+    package = pkgs.mongodb-7_0;
+    user = "mongodb";
+    extraConfig = ''
+      replication:
+        replSetName: dbdiagramrs1
+    '';
+  }; 
+  environment.systemPackages = with pkgs; [
+    mongosh
+  ];
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
